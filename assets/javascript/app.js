a7ac569d027c98e16f9b670ecec492530521c8a2
Xander C <xander.canedo@gmail.com>
2019-10-02 23:13:35 -0600

    got basic HTML wireframe

---
+++ assets/javascript/app.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDvMUSMjpqMupxYUv_QKn_iJEzEk_9MP0M",
+    authDomain: "trainscheduler-ee589.firebaseapp.com",
+    databaseURL: "https://trainscheduler-ee589.firebaseio.com",
+    projectId: "trainscheduler-ee589",
+    storageBucket: "",
+    messagingSenderId: "525665946610",
+    appId: "1:525665946610:web:0edef019c06b94d936b96b",
+    measurementId: "G-3VZFW51M1V"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
